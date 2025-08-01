6fcbdb8169269b7febaa6af25158fba8a3d798a8
Brayan <iglesiasbrayan2@gmail.com>
2025-08-01 16:35:00 +0200

    Creado componente Background

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { BackgroundComponent } from './background/background.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    BackgroundComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    BackgroundComponent
   ]
 })
 export class ComponentsModule { }
